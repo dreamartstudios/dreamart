8a2db4b5d7f26791d0bc4692c909e98718d24eec
dreamartstudios <studiosdreamart@gmail.com>
2020-12-28 21:44:02 +0530

    create navbar, about us section and footer

--- index.php
+++ index.php
@@ -1,11 +1,138 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="dist/css/style.css">
+
+
+    <title>Dreamart studios</title>
+  </head>
+  <body class="abc">
+   <header class="dreamart-header">
+   <div class="container">
+   <div class="row">
+   <div class="col-md-12">
+   <div class="text-center">
+   <img src="dist/images/dreamart-logo.svg" alt="" class="img-fluid my-5">
+   </div>
+   </div>
+   </div>
+   </div>
+   <!-- end logo  -->
+
+   <!-- navigation bar  -->
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+  <div class="container-fluid">
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link dreamart-link"  href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dreamart-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dreamart-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dreamart-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dreamart-link" href="#">Blog</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link dreamart-link" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+        </li>
+      </ul>
+      
+     
+    </div>
+  </div>
+</nav>
+
+   <!-- end navigation  -->
+   </header>
+   <!-- end header  -->
+
+
+   <section class="dreamart-about" style="margin-top:50px; margin-bottom:50px;">
+       <div class="container">
+           
+           
+           <div class="row">
+               <div class="col-md-5">
+                   <div class="text-center">
+                       <img src="dist/images/about-me.svg" alt="" class="img-fluid">
+                   </div>
+               </div>
+               <div class="col-md-7">
+                   <div class="text-center p-2">
+                   <div class="h2 my-2">I'm a Passionate designer & developer who loves simplicity in 
+                       things and crafts beautiful user interfaces with love.</div>
+
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit corporis veniam eos 
+                       delectus odio itaque dolores illum incidunt. Doloremque ipsa veritatis libero quos
+                        tempora vitae praesentium, voluptatum sequi, labore quia, dicta cum facilis laborum a 
+                        officia quidem possimus obcaecati? Cumque repellendus ratione quidem ipsum amet ad maiores,
+                         deserunt quis quae tenetur non dolore, dicta, incidunt ipsam quo voluptas nostrum! Quisquam 
+                         maiores, quam beatae placeat unde quod nobis deleniti in labore tempore, cupiditate, fugiat
+                          id. Nulla sequi dolorem vitae? Quis, recusandae! Perspiciatis quasi illum recusandae eveniet
+                           numquam et delectus ipsa odio.
+                        Voluptate nesciunt voluptas dolorem quam iste mollitia eius? Aliquam, cumque!</p>
+                        <button class="btn btn-danger">about me</button>
+
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- end about us section  -->
+
+
+
     
+<footer class="dreamart-footer bg-dark">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-6">
+<div class="text-center">
+<ul class="nav justify-content-center">
+<li class="nav-item"><a href="" class="nav-link dreamart-footer-link"><i class="fab fa-facebook-f"></i></a></li>
+<li class="nav-item"><a href="" class="nav-link dreamart-footer-link"><i class="fab fa-twitter"></i></a></li>
+<li class="nav-item"><a href="" class="nav-link dreamart-footer-link"><i class="fab fa-instagram"></i></a></li>
+<li class="nav-item"><a href="" class="nav-link dreamart-footer-link"><i class="fab fa-github"></i></a></li>
+<li class="nav-item"><a href="" class="nav-link dreamart-footer-link"><i class="fab fa-youtube"></i></a></li>
+</ul>
+</div>
+</div>
+<div class="col-md-6">
+<div class="text-center">
+<div class="theme-option">
+  <div id="theme-default"  class=""></div>
+  <div id="theme-dark"  class=""></div>
+  <div id="theme-green"  class=""></div>
+  <div id="theme-red"  class=""></div>
+  <div id="theme-yellow"  class=""></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" integrity="sha512-F5QTlBqZlvuBEs9LQPqc1iZv2UMxcVXezbHzomzS6Df4MZMClge/8+gXrKw2fl5ysdk4rWjR0vKS7NNkfymaBQ==" crossorigin="anonymous"></script>
 </body>
 </html>
